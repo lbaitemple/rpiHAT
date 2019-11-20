87e56b59f7f3771567b8ee0cf69f1fe0db67562d
lbaitemple <lbai@temple.edu>
2019-11-20 09:33:24 -0500

    Update and rename testservono.py to testservont.py

---
+++ testservont.py
@@ -0,0 +1,12 @@
+# test no threading 
+from rpiHAT import ServoNT
+import time
+
+s=ServoNT(channel=1, freq=98.1)
+s.set(0.1)
+while True:
+  try
+    time.sleep(0.5)
+  except (KeyboardInterrupt, SystemExit):
+    s.set(0.15)
+    
