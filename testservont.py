2fa95900e11c49dbd6182702ff68e9a0e7663068
lbaitemple <lbai@temple.edu>
2020-02-10 10:46:15 -0500

    Update testservont.py

--- testservont.py
+++ testservont.py
@@ -5,8 +5,9 @@ import time
 s=ServoNT(channel=1, freq=98.1)
 s.set(0.1)
 while True:
+  time.sleep(0.5)
   try:
-    time.sleep(0.5)
+    pass
   except (KeyboardInterrupt, SystemExit):
     s.set(0.15)
     
