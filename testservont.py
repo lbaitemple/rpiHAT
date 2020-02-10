894d6fc9d8f59c4f7b29097f48abb09b9517d08c
lbaitemple <lbai@temple.edu>
2020-02-10 14:26:12 -0500

    Update testservont.py

--- testservont.py
+++ testservont.py
@@ -2,12 +2,11 @@
 from rpiHAT import ServoNT
 import time
 
-s=ServoNT(channel=1, freq=98.1)
+s=ServoNT(channel=1, freq=92.7)
 s.pulse(0.1)
-while True:
-  time.sleep(0.5)
-  try:
-    pass
-  except (KeyboardInterrupt, SystemExit):
+
+try:
+    while True:
+        time.sleep(0.5)
+except (KeyboardInterrupt, SystemExit):
     s.pulse(0.15)
-    
