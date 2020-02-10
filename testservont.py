4ba6e164723bd4f70cf131aa4eab04a6642dcb6e
lbaitemple <lbai@temple.edu>
2020-02-10 14:17:00 -0500

    Update testservont.py

--- testservont.py
+++ testservont.py
@@ -3,11 +3,11 @@ from rpiHAT import ServoNT
 import time
 
 s=ServoNT(channel=1, freq=98.1)
-s.set(0.1)
+s.pulse(0.1)
 while True:
   time.sleep(0.5)
   try:
     pass
   except (KeyboardInterrupt, SystemExit):
-    s.set(0.15)
+    s.pulse(0.15)
     
